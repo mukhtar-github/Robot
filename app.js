f873d964b689afe70a6ecb7f8262fa511dc30514
[Mukhtar Dev] <[mukhtartanimugarba@gmail.com]>
2020-08-23 16:44:38 +0100

    Updated Files

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const mask = document.querySelector('.mask');
     const torso = document.querySelector('.torso');
     const robot = document.querySelector('.robot');
-    
+    let position = 0;
+
     function changeEye() {
         eyes.forEach(eye => eye.classList.toggle('blue-eye'));
     }
 
     mask.addEventListener('click', changeEye);
+
+    function moveRobot() {
+        position += 50;
+        robot.style.left = position + 'px';
+    }
+
+    torso.addEventListener('click', moveRobot);
 });
